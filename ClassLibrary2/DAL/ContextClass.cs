45f2fcadf29e025e6a94961c2791a6126354c2f6
Curtis Engel <cengel2@studentmail.nait.ca>
2018-09-16 10:39:25 -0600

    Updated Class Library2 Added DAL/ContextClass

--- ClassLibrary2/DAL/ContextClass.cs
+++ ClassLibrary2/DAL/ContextClass.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary2
+namespace ContextClass
 {
-    public class Class1
+    public class ContextClass
     {
     }
 }
