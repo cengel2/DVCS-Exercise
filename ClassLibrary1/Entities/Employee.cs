c842087382b0b179935761adfc91c53edccc6936
Curtis Engel <cengel2@studentmail.nait.ca>
2018-09-16 10:34:39 -0600

    Updated ClassLibrary1 - Added Entities/Employee

--- ClassLibrary1/Entities/Employee.cs
+++ ClassLibrary1/Entities/Employee.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary1
+namespace Employee
 {
-    public class Class1
+    public class Employee
     {
     }
 }
